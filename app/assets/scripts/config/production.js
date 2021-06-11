0dc68a890188c3f533657fe9890303ee3ef6193f
Aimee Barciauskas <aimee@developmentseed.org>
2021-06-10 18:43:29 -0700

    Fix api_url configuration

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -1,4 +1,11 @@
 // module exports is required to be able to load from gulpfile.
+const api_url = () => {
+  if (process.env.STAGE != null) {
+    return process.env[`${process.env.STAGE.replace('-', '_').toUpperCase()}_API_URL`]
+  } else {
+    return process.env.API_URL || 'http://localhost:8000/v1';
+  }
+}
 module.exports = {
   default: {
     environment: 'production',
@@ -6,7 +13,7 @@ module.exports = {
     appDescription: 'Explore the data using our new experimental dashboard.',
     twitterHandle: '@NASAEarthData',
     mbToken: 'pk.eyJ1IjoiY292aWQtbmFzYSIsImEiOiJja2F6eHBobTUwMzVzMzFueGJuczF6ZzdhIn0.8va1fkyaWgM57_gZ2rBMMg',
-    api: process.env[`${process.env.STAGE.replace('-', '_').toUpperCase()}_API_URL`] || 'http://localhost:8000',
+    api: api_url,
     map: {
       center: [0, 0],
       zoom: 2,
